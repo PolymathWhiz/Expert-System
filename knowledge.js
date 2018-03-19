15f1ad82125fa581a1c5865d73b59cc6379b25bc
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-03-19 16:05:49 +0000

    taking shape

--- knowledge.js
+++ knowledge.js
@@ -1,8 +1,28 @@
 const questions = [
-  'What will you like to invest in?',
-  'How much do you have to invest?'
+  'What kind of business would you like to invest in?',
+  'How much do you have to invest in business?',
+  'How long do you want to invest for?'
 ];
 
-const options = [
-  'Treasury Bonds', 'Investment Funds', 'Annunities', 'Savings for collegs', 'Retirements', 'Security', 'Insurance', 'Real Estate', 'IT products and services'
+const investmentOptions = [
+  'Treasury Bonds', 'Retirements', 'Security', 'Insurance', 'Real Estate', 'IT products and services'
 ];
+
+const lengthInYears = {
+  'Six months': 6,
+  'One year': 12,
+  'Five years': 60,
+  'Ten years': 120
+};
+
+function getFeedback(feedback) {
+  return feedback;
+}
+
+function guessOption(option) {
+  switch (option) {
+    case investmentOptions[0]:
+
+      break;
+  }
+}
