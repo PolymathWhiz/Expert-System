0a0d584e6f97c84830bcd8a3f7ad33378a1654fe
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-03-19 15:34:13 +0000

    initial commit

---
+++ knowledge.js
@@ -0,0 +1,8 @@
+const questions = [
+  'What will you like to invest in?',
+  'How much do you have to invest?'
+];
+
+const answers = [
+  'Treasury Bonds', 'Investment Funds', 'Annunities', 'Savings for collegs', 'Retirements', 'Security', 'Insurance', 'Real Estate', 'IT products and services'
+];
