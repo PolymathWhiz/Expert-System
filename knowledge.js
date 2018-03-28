357792f072e17fdcb0eb4d471805a36f347c97aa
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-03-28 07:39:17 +0000

    about to push to heroku

--- knowledge.js
+++ knowledge.js
@@ -16,28 +16,52 @@ const lengthInYears = {
 function guessOption(sector, budget, duration, amountLeft) {
   switch (sector) {
     case sectors[0]:
-      feedback.html('You selected ' + sectors[0]);
+      if ((amountLeft >= 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        feedback.html(`You can invest in ${investmentOptions[3]}`);
+      } else if ((amountLeft < 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        feedback.html(`You can invest in ${investmentOptions[1]}`);
+      } else {
+        feedback.html(`Your capital is not enough to invest in this sector`);
+      }
       break;
     case sectors[1]:
-      if (budget > 0 && budget < 1000) {
-        if (duration <= lengthInYears["Six months"]) {
-          feedback.html('Invest in ' + investmentOptions[1]);
-        }
+      if ((amountLeft >= 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        console.log('Reached here');
+      } else if ((amountLeft < 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        feedback.html(`You can invest in ${investmentOptions[1]}`);
       } else {
-        feedback.html('No investment options found. Try again!');
+        feedback.html('Your capital is not enough to invest in this sector');
       }
       break;
     case sectors[2]:
-      feedback.html('you selected ' + sectors[2]);
+      if ((amountLeft >= 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        console.log('Reached here');
+      } else if ((amountLeft < 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        feedback.html(`You can invest in ${investmentOptions[1]}`);
+      } else {
+        feedback.html('Your capital is not enough to invest in this sector');
+      }
       break;
     case sectors[3]:
-      feedback.html('you selected ' + sectors[3]);
+      if ((amountLeft >= 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        console.log('Reached here');
+      } else if ((amountLeft < 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        feedback.html(`You can invest in ${investmentOptions[1]}`);
+      } else {
+        feedback.html('Your capital is not enough to invest in this sector');
+      }
       break;
     case sectors[4]:
-      feedback.html('you selected ' + sectors[4]);
+      if ((amountLeft >= 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        console.log('Reached here');
+      } else if ((amountLeft < 1000 && isMoneyAvaliable(budget, amountLeft))) {
+        feedback.html(`You can invest in ${investmentOptions[1]}`);
+      } else {
+        feedback.html('Your capital is not enough to invest in this sector');
+      }
       break;
     default:
-      feedback.html('Invalid input');
+      feedback.html('Invalid request');
       break;
   }
 }
@@ -46,8 +70,15 @@ function amountLeft(monthlyEarnings, monthlySpendings) {
   var amountLeft = monthlyEarnings - monthlySpendings;
 
   if (amountLeft < 1) {
-    return feedback.html(`You don't have enough money`);
+    feedback.html(`You don't have enough money`);
+    return;
   } else return amountLeft;
+}
+
+function isMoneyAvaliable(budget, amountLeft) {
+  if (amountLeft > budget || amountLeft == budget) {
+    return true;
+  } else return false;
 }
 
 $('#predict').click(function () {
@@ -57,14 +88,20 @@ $('#predict').click(function () {
     duration = $('#length').val(),
     reason = $('#reason').val(),
     incomeSource = $('#incomeSource').val(),
-    monthlyEarnings = $('#montlyEarnings').val(),
+    monthlyEarnings = $('#monthlyEarnings').val(),
     monthlySpendings = $('#monthlySpendings').val(),
     investedStatus = $('#investedStatus').val(),
     goals = $('#goals').val(),
     investmentPlan = $('#investmentPlan').val();
 
-  guessOption(selected, budget, duration, amountLeft(monthlyEarnings, monthlySpendings));
+  if (selected == '' || budget == '' || duration == '' || monthlyEarnings == '' || monthlySpendings == '') {
+    $('.error').html('Fill in all fields');
+    return;
+  }
 
+  var remainingAmount = amountLeft(monthlyEarnings, monthlySpendings);
+
+  guessOption(selected, budget, duration, remainingAmount);
 });
 
 $('#reset').click(function () {
